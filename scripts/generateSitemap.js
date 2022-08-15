bcab3327ec2c2b2a25e7b1924d8974bad0e1f643
Kolton Bloom <bloomkd46@icloud.com>
2022-08-15 10:35:31 -0700

    updated sitemap script

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -8,4 +8,10 @@ const routes = ['home', 'news', 'sponsors', 'contact-us', 'programs/minecraft', 
 
 const sitemap = [...routes, ...posts.map(post => `news/${post.title.split(' ').join('-')}`)];
 
-fs.writeFileSync(path.join(__dirname, '../src/assets/sitemap.xml'), sitemap.map(route => `https://4h-botsmiths.github.io/wesbite/${route}`).join('\n'));
+fs.writeFileSync(path.join(__dirname, '../src/assets/sitemap.xml'),
+  `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${sitemap.map(route =>
+    `<url>
+      <loc>https://4h-botsmiths.github.io/wesbite/${route}</loc>
+    </url>`).join('\n')}
+</urlset>`);
