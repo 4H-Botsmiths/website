e90677faad6f1510766d28b54988b9dd7d82e9a0
Kolton Bloom <bloomkd46@icloud.com>
2022-08-15 11:30:42 -0700

    updated sitemap

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const posts = require('../src/app/news/news.json');
-const routes = ['', 'news', 'sponsors', 'contact-us', 'programs/minecraft', 'programs/fll/explore', 'programs/fll/challenge', 'programs/ftc', 'programs/frc'];
+const routes = ['', 'news', 'sponsors', 'sponsors/options', 'sponsors/contact-us', 'contact-us', 'programs/minecraft', 'programs/fll/explore', 'programs/fll/challenge', 'programs/ftc', 'programs/frc'];
 
 const sitemap = [...routes, ...posts.map(post => `news/${post.title.split(' ').join('-')}`)];
 
