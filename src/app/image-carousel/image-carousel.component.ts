3849abb6ebb1a4d4a86b56a3b7b11bddec71c4ec
Kolton Bloom <bloomkd46@icloud.com>
2022-08-16 12:43:25 -0700

    improved carousel component

--- src/app/image-carousel/image-carousel.component.ts
+++ src/app/image-carousel/image-carousel.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NavigationStart, Router } from '@angular/router';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { ColorSchemeService } from '../color-scheme.service';
 import { ImageSync } from '../image-fetcher.service';
@@ -12,39 +12,26 @@ import { ImageSync } from '../image-fetcher.service';
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.scss']
 })
-export class ImageCarouselComponent implements OnInit {
+export class ImageCarouselComponent implements OnInit, OnDestroy {
 
   /** The Images To Display*/
   @Input() images!: ImageSync[];
+
+  private interval?: NodeJS.Timer;
 
   public carouselName = 'imageCarousel' + Math.round(Math.random() * 10);
 
   constructor(public colorScheme: ColorSchemeService, private router: Router) {
   }
 
+
   ngOnInit(): void {
-    //document.getElementById('nextButton')!.click()
-    //document.onload = () => document.getElementById('nextButton')!.click()
-    //setTimeout(() => document.getElementById('nextButton')!.click(), 1000);
-    //const myCarousel = document.querySelector('#' + this.carouselName);
-    //const carousel: Carousel = new window.bootstrap.Carousel(myCarousel!)
-    //setTimeout(() => document.getElementById('nextButton')!.dispatchEvent(new Event('click')), 1000);
-    //document.getElementById('nextButton')!.dispatchEvent(new Event('click'))
-    /*setTimeout(() => {
-      console.log('rotating')
-      //carousel.next()
-      document.getElementById(this.carouselName + 'nextButton')!.click()
-    }, 5000);*/
-    const interval = setInterval(() => {
+    this.interval = setInterval(() => {
       console.log('rotating');
       document.getElementById('nextButton')?.click();
-      //$("#nextButton").trigger('click');
     }, 1500);
-    this.router.events.subscribe((event: any) => {
-      if (event instanceof NavigationStart) {
-        clearInterval(interval);
-        // Show progress spinner or progress bar
-      }
-    });
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
   }
 }
