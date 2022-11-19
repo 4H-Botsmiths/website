fdf8009cebd3f0ce12cc69e35beab0c02d94345c
Kolton Bloom <bloomkd46@icloud.com>
2022-11-18 19:57:02 -0800

    improved sponsor page modal

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -1,3 +1,5 @@
+import * as Bootstrap from 'bootstrap';
+
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
@@ -8,6 +10,7 @@ import sponsors from './sponsors.json';
 
 
 
+declare const bootstrap: typeof Bootstrap;
 @Component({
   selector: 'app-sponsors',
   templateUrl: './sponsors.component.html',
@@ -25,35 +28,45 @@ export class SponsorsComponent implements OnInit, AfterViewInit {
     amount: 0,
     additionalInfo: ''
   };
+  interval!: NodeJS.Timer;
   constructor(public colorScheme: ColorSchemeService, public imageFetcher: ImageFetcherService, public router: Router) { }
 
   /**
    * add sponsor options Modal listeners
    */
   ngOnInit(): void {
-    const interval = setInterval(() => {
+    this.interval = setInterval(() => {
       this.form.valid = this.form.companyName !== '' && this.form.amount >= 100 && (this.form.companyLogo ? this.isValidUrl(this.form.companyLogo) : true);
     });
+  }
+  @ViewChild("modal") modal!: ElementRef;
+
+  /**
+   * Open sponsor options modal if needed
+   */
+  ngAfterViewInit(): void {
+    const modal = new bootstrap.Modal(this.modal.nativeElement);
     this.router.events.subscribe((event: any) => {
       if (event instanceof NavigationStart) {
-        clearInterval(interval);
+        clearInterval(this.interval);
+        this.modal.nativeElement.rem;
         // Show progress spinner or progress bar
       }
       if (event instanceof NavigationEnd) {
         // Hide progress spinner or progress bar
         if (this.routerLinkActive('/sponsors/options') || this.routerLinkActive('/sponsors/contact-us')) {
-          this.openModal();
+          modal.show();
         }
       }
     });
-  }
-  /**
-   * Open sponsor options modal if needed
-   */
-  ngAfterViewInit(): void {
+    this.modal.nativeElement.addEventListener('hidden.bs.modal', () => {
+      clearInterval(this.interval);
+      this.router.navigate(['/sponsors']);
+    });
     if (this.routerLinkActive('/sponsors/options') || this.routerLinkActive('/sponsors/contact-us')) {
-      this.openModal();
+      modal.show();
     }
+
   }
   /**
  * Returns wether or not a link is active
@@ -62,6 +75,7 @@ export class SponsorsComponent implements OnInit, AfterViewInit {
  */
   routerLinkActive(link: string): boolean {
     return this.router.isActive(link, { paths: 'subset', queryParams: 'subset', fragment: 'ignored', matrixParams: 'ignored' });
+
   }
   /**
 * Returns wether or not a link is valid
@@ -70,17 +84,6 @@ export class SponsorsComponent implements OnInit, AfterViewInit {
 */
   isValidUrl(link: string): boolean {
     return /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(link);
-  }
-
-  @ViewChild("sponsorModal") sponsorModal!: ElementRef;
-  @ViewChild("sponsorModalButton") sponsorModalButton!: ElementRef;
-  /**
-   * Open the sponsorship options modal
-   */
-  openModal() {
-    if (!this.sponsorModal.nativeElement?.classList.contains('show')) {
-      this.sponsorModalButton.nativeElement?.click();
-    }
   }
 }
 interface Option {
