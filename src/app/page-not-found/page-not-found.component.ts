412a3acff45aea89106df75dcc5554de75015c0a
Kolton Bloom <bloomkd46@icloud.com>
2022-11-18 18:12:37 -0800

    added program redirects

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 import { ColorSchemeService } from '../color-scheme.service';
 
@@ -11,14 +12,47 @@ import { ColorSchemeService } from '../color-scheme.service';
   styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit, OnDestroy {
+  public countdown = 4;
+  public url?: string;
+  public redirect = false;
 
-  constructor(public colorScheme: ColorSchemeService, private meta: Meta) { }
+  constructor(public colorScheme: ColorSchemeService, private meta: Meta, private router: Router) { }
 
   /**
    * mark page as none indexable
    */
   ngOnInit(): void {
     this.meta.addTag({ name: 'robots', content: 'NOINDEX' });
+    const url = this.router.url;
+    if (url.includes('frc')) {
+      this.prepareRedirect('/programs/frc');
+    } else if (url.includes('ftc')) {
+      this.prepareRedirect('/programs/ftc');
+    } else if (url.includes('fll')) {
+      if (url.includes('explore')) {
+        this.prepareRedirect('/programs/fll/explore');
+      } else {
+        this.prepareRedirect('/programs/fll/challenge');
+      }
+    } else if (url.includes('minecraft')) {
+      this.prepareRedirect('/programs/minecraft');
+    }
+  }
+  prepareRedirect(url: string): void {
+    this.url = url;
+    this.redirect = true;
+    this.countdownRedirect();
+  }
+  countdownRedirect() {
+    if (!this.redirect) return;
+    this.countdown!--;
+    setTimeout(() => {
+      if (this.countdown <= 0) {
+        this.router.navigate([this.url!]);
+      } else {
+        this.countdownRedirect();
+      }
+    }, 1000);
   }
   /**
    * remove non-indexable tag
